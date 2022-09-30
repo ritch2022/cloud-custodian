7d215aecc5c6fd3fe3c07a3b873228aaffc68478
ritchiecai <ritchiecai@tencent.com>
2022-09-30 14:47:13 +0800

    refactor: optimize date str format func

--- tools/c7n_tencentcloud/c7n_tencentcloud/resources/nat-gateway.py
+++ tools/c7n_tencentcloud/c7n_tencentcloud/resources/nat-gateway.py
@@ -1,6 +1,5 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
-import pytz
 from c7n_tencentcloud.provider import resources
 from c7n_tencentcloud.query import ResourceTypeInfo, QueryResourceManager
 from c7n_tencentcloud.utils import PageMethod, isoformat_date_str
@@ -23,11 +22,6 @@ class NatGateway(QueryResourceManager):
         taggable = True
 
     def augment(self, resources):
-        from_tz = pytz.timezone("Asia/Shanghai")
         for resource in resources:
-            isoformat_date_str(resource,
-                               ["CreatedTime"],
-                               "%Y-%m-%d %H:%M:%S",
-                               from_tz,
-                               pytz.utc)
+            isoformat_date_str(resource, ["CreatedTime"], "%Y-%m-%d %H:%M:%S")
         return resources
