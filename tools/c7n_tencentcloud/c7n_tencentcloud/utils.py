fd9a22deaff8fe89da870000ba18c418c1fd2e5c
ritchiecai <ritchiecai@tencent.com>
2022-09-30 09:55:33 +0800

    refactor: a common date format func

--- tools/c7n_tencentcloud/c7n_tencentcloud/utils.py
+++ tools/c7n_tencentcloud/c7n_tencentcloud/utils.py
@@ -1,6 +1,6 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
-
+from datetime import datetime
 from enum import Enum
 
 
@@ -13,3 +13,17 @@ class PageMethod(Enum):
     """
     Offset = 0
     PaginationToken = 1
+
+
+def isoformat_date_str(data: dict,
+                      field_keys: list,
+                      original_date_str_format: str,
+                      timezone_from,
+                      timezone_to):
+    """
+    standardize the date string, using isoformat including timezone info
+    example: '2022-09-28T15:28:28+00:00'
+    """
+    for key in field_keys:
+        dt = timezone_from.localize(datetime.strptime(data[key], original_date_str_format))
+        data[key] = dt.astimezone(timezone_to).isoformat()
