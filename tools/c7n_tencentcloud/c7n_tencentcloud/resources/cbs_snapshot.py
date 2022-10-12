551ece97a7554aead79f3879fe9efc195ace8849
ritchiecai <ritchiecai@tencent.com>
2022-10-12 15:53:03 +0800

    chore: add metadata for datetime format

--- tools/c7n_tencentcloud/c7n_tencentcloud/resources/cbs_snapshot.py
+++ tools/c7n_tencentcloud/c7n_tencentcloud/resources/cbs_snapshot.py
@@ -1,8 +1,9 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
+import pytz
 from c7n_tencentcloud.provider import resources
 from c7n_tencentcloud.query import ResourceTypeInfo, QueryResourceManager
-from c7n_tencentcloud.utils import PageMethod, isoformat_date_str
+from c7n_tencentcloud.utils import PageMethod, isoformat_datetime_str
 
 
 @resources.register("cbs-snapshot")
@@ -20,8 +21,15 @@ class CBSSnapshot(QueryResourceManager):
         paging_def = {"method": PageMethod.Offset, "limit": {"key": "Limit", "value": 20}}
         resource_prefix = "volume"
         taggable = True
+        datetime_fields_format = {
+             "CreateTime": ("%Y-%m-%d %H:%M:%S", pytz.timezone("Asia/Shanghai"))
+         }
+
 
     def augment(self, resources):
         for resource in resources:
-            isoformat_date_str(resource, ["CreateTime"], "%Y-%m-%d %H:%M:%S")
+            field_format = self.resource_type.datetime_fields_format["CreateTime"]
+            resource["CreateTime"] = isoformat_datetime_str(resource["CreateTime"],
+                                                            field_format[0],
+                                                            field_format[1])
         return resources
--- tools/c7n_tencentcloud/c7n_tencentcloud/resources/nat_gateway.py
+++ tools/c7n_tencentcloud/c7n_tencentcloud/resources/nat_gateway.py
@@ -1,8 +1,9 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
+import pytz
 from c7n_tencentcloud.provider import resources
 from c7n_tencentcloud.query import ResourceTypeInfo, QueryResourceManager
-from c7n_tencentcloud.utils import PageMethod, isoformat_date_str
+from c7n_tencentcloud.utils import PageMethod, isoformat_datetime_str
 
 
 @resources.register("nat-gateway")
@@ -20,8 +21,14 @@ class NatGateway(QueryResourceManager):
         paging_def = {"method": PageMethod.Offset, "limit": {"key": "Limit", "value": 20}}
         resource_prefix = "nat"
         taggable = True
+        datetime_fields_format = {
+             "CreatedTime": ("%Y-%m-%d %H:%M:%S", pytz.timezone("Asia/Shanghai"))
+         }
 
     def augment(self, resources):
         for resource in resources:
-            isoformat_date_str(resource, ["CreatedTime"], "%Y-%m-%d %H:%M:%S")
+            field_format = self.resource_type.datetime_fields_format["CreatedTime"]
+            resource["CreatedTime"] = isoformat_datetime_str(resource["CreatedTime"],
+                                                             field_format[0],
+                                                             field_format[1])
         return resources
