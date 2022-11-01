bc10acfc5373eea835a48703d29ec1a789c7585f
wenlongmma@tencent.com <wenlongmma@tencent.com>
2022-11-01 15:11:58 +0800

    feat: just for ci

--- tools/c7n_tencentcloud/c7n_tencentcloud/resources/resource_map.py
+++ tools/c7n_tencentcloud/c7n_tencentcloud/resources/resource_map.py
@@ -13,6 +13,6 @@ ResourceMap = {
     "tencentcloud.cls": "c7n_tencentcloud.resources.cls.LogTopic",
     "tencentcloud.tcr": "c7n_tencentcloud.resources.tcr.TCR",
     "tencentcloud.security-group": "c7n_tencentcloud.resources.security_group.SecurityGroup",
-    "tencentcloud.mysql": "c7n_tencentcloud.resources.mysql.MySQL",
-    "tencentcloud.mysql-backup": "c7n_tencentcloud.resources.mysql_backup.MySQLBackUp"
+    "tencentcloud.mysql-backup": "c7n_tencentcloud.resources.mysql_backup.MySQLBackUp",
+    "tencentcloud.mysql": "c7n_tencentcloud.resources.mysql.MySQL"
 }
